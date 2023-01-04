e5232d85a6d0896611dfbf6e5e47f746d9241043
JoaoAfranio <joaoafraniorosa@hotmail.com>
2023-01-04 14:09:00 -0300

    feat: get all posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,5 @@
-import { response } from "express";
 import postsRepository from "../repositories/postsRepository.js";
+import urlMetadata from "url-metadata";
 
 export async function likePost(req, res) {
   try {
@@ -17,6 +17,24 @@ export async function deslikePost(req, res) {
     await postsRepository.deleteLike(res.locals.userId, req.params.id);
 
     res.send(204);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+
+export async function getAllPosts(req, res) {
+  try {
+    const { rows } = await postsRepository.getAllPosts();
+
+    for(const post of rows) {
+      const metadata = await urlMetadata(post.url);
+      post.title = metadata.title;
+      post.image = metadata.image;
+      post.linkDescription = metadata.description;
+    }
+
+    res.send(rows);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
